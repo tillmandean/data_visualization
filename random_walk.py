deae491cba48f96f5242c210bd16ad7dc7704645
Tillman <tillman.dean@comcast.net>
2018-08-13 15:00:18 -0500

    Changes to random walk

--- random_walk.py
+++ random_walk.py
@@ -1,6 +1,6 @@
 from random import choice 
 
-class RadomWalk():
+class RandomWalk():
     """A class to generate random walks."""
 
     def __init__(self, num_points=5000):
@@ -18,11 +18,11 @@ class RadomWalk():
         while(len(self.x_values) < self.num_points):
 
             # Decide which direction to go and how far to go in that direction
-            x_direction = choice(1, -1)
+            x_direction = choice([1, -1])
             x_distance = choice([0, 1, 2, 3, 4])
             x_step = x_direction * x_distance
 
-            y_direction = choice(1, -1)
+            y_direction = choice([1, -1])
             y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
 
