040dbc6baf98966776f2d0a15bfe4e1420f03950
Tillman <tillman.dean@comcast.net>
2018-08-13 15:42:29 -0500

    Chapter 15 complete

--- rw_visual.py
+++ rw_visual.py
@@ -5,12 +5,15 @@ from random_walk import RandomWalk
 # Keep making walks as long as the program is active.
 while True: 
     # Make a random walk, and plot the points.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # Set the size of the plotting window.
+    plt.figure(figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-        edgecolor='none', s=15)
+        edgecolor='none', s=1)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
